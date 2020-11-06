015269e7483334241d735129d53e65e7b2c2def9
yoza001 <72470979+yoza001@users.noreply.github.com>
2020-11-06 11:42:52 -0800

    done

--- sevenless.py
+++ sevenless.py
@@ -4,3 +4,15 @@ start = 0
 end   = 99
 divisor=7
 print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
+
+a=0
+b=0
+while a<100:
+  if ((b%7)==0) :
+      b=b+1
+  else :
+     print (b)
+      b=b+1
+  a=a+1
+  
+
